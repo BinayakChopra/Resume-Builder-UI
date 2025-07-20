5d455c87856d9e9b974c06ebc2bbcbceebbb5ad3
Binayak <binayakchopra34@gmail.com>
2025-07-20 14:54:57 +0530

    Fix path to autoloader.php in login.php

--- login.php
+++ login.php
@@ -3,7 +3,8 @@
 <html>
 <head>
 	<title>Online Resume Builder</title>
-<?php require "assets/autoloader.php"?>
+<?php require "autoloader.php" ?>
+
 	<style type="text/css" id="#some2">
 <?php include 'css/myStyle.css';?>
 *{
